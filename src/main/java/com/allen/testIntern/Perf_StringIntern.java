6cd63cc913b8ac18a9edeffc3f3eb50044d81d12
Allencs <allenchensheng@163.com>
2022-09-26 00:12:37 +0800

    添加intern优化案例

---
+++ src/main/java/com/allen/testIntern/Perf_StringIntern.java
@@ -0,0 +1,40 @@
+package com.allen.testIntern;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @Author: allen
+ * @Date: 2022/9/23 13:23
+ * @Description:
+ */
+public class Perf_StringIntern {
+    public static ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(10000000);
+
+    public static void generateString() {
+        Integer[] data = new Integer[10];
+        Random random = new Random(10 * 10000);
+        for (int i = 0; i< data.length; i++){
+            data[i] = random.nextInt();
+        }
+        for (int i =0; i < 10000000; i++) {
+            try {
+                arrayBlockingQueue.put(new String(String.valueOf(data[i % data.length])));
+//                arrayBlockingQueue.put(new String(String.valueOf(data[i % data.length])).intern());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            System.out.println("创建完毕");
+            Thread.sleep(60 * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Perf_StringIntern.generateString();
+    }
+}
