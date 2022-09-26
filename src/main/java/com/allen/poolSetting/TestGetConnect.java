6676679c4224e5b2f26e1f5929aeaf16854a777d
Allencs <allenchensheng@163.com>
2022-09-26 21:31:47 +0800

    连接池案例理解

---
+++ src/main/java/com/allen/poolSetting/TestGetConnect.java
@@ -0,0 +1,66 @@
+package com.allen.poolSetting;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: allen
+ * @Date: 2022/9/26 21:15
+ * @Description: 连接池测试案例
+ **/
+public class TestGetConnect {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestGetConnect.class);
+    private final HikariDataSource dataSource;
+
+    public TestGetConnect() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://10.10.220.15:3306/PerfCase?characterEncoding=utf-8&useSSL=false");
+        config.setUsername("root");
+        config.setPassword("Perfma888.");
+        config.setMaximumPoolSize(5);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(90000);
+        dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        TestGetConnect testGetConnect = new TestGetConnect();
+        ExecutorService executorService = new ThreadPoolExecutor(10, 10, 0L,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        int i = 0;
+        while (i < 10000) {
+            executorService.submit(() -> {
+                try {
+                    Connection conn = testGetConnect.getConnection();
+                    logger.info("线程：「{}」得到连接，开始查询...", Thread.currentThread().getName());
+                    Thread.sleep(1000);
+                    logger.info("线程：「{}」查询结束。", Thread.currentThread().getName());
+                    conn.close();
+                } catch (SQLException | InterruptedException throwables) {
+                    throwables.printStackTrace();
+                }
+            });
+            i++;
+        }
+        executorService.shutdown();
+    }
+}
